138541e550cf39f037c7df42563f02707bc20f05
anshul <anshhulkaushik@gmail.com>
2022-07-13 13:35:34 +0530

    wed

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,3 +123,18 @@ const myFunction = () => {
   }
 
 
+  function increaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+    console.log(value);
+  }
+  
+  function decreaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('number').value = value;
+  }
